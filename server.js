5bcc3246efea5dc5d1daea7c574ad632f8ecf370
ForgeBrew96 <christianbmendoza96@gmail.com>
2024-10-15 16:29:08 -0400

    final changes before controller

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const bodyParser = require('body-parser');
 const logger = require('morgan');
 const { Category, Product } = require('./models')
 
-// const categoryController = require('./controllers/categoryController');
-// const productController = require('./controllers/productController');
+const categoryController = require('./controllers/categoryController');
+const productController = require('./controllers/productController');
 
 
 const PORT = process.env.PORT || 3001;
@@ -21,7 +21,7 @@ app.use(bodyParser.json())
 
 async function main() {
     try {
-        await db.connect(); // Ensure this is in place to connect to your DB
+        await db.connect();
         console.log('Database connected successfully');
     } catch (error) {
         console.error('Database connection error:', error);
@@ -36,14 +36,20 @@ app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
 
 app.get('/', (req, res) => res.send('This is our landing page!'))
 
-// app.get('/categories', categoryController.getAllCategories)
-// app.get('/categories/:id', categoryController.getCategoryById)
-// app.post('/categories', categoryController.createCategory)
-// app.put('/categories/:id', categoryController.updateCategory)
-// app.delete('/categories/:id', categoryController.deleteCategory)
+app.get('/categories', categoryController.getAllCategories)
+app.get('/categories/:id', categoryController.getCategoryById)
+app.post('/categories', categoryController.createCategory)
+app.put('/categories/:id', categoryController.updateCategory)
+app.delete('/categories/:id', categoryController.deleteCategory)
 
-// app.get('/products', productController.getAllProducts)
-// app.get('/products/:id', productController.getProductById)
-// app.post('/products', productController.createProduct)
-// app.put('/products/:id', productController.updateProduct)
-// app.delete('/products/:id', productController.deleteProduct)
+app.get('/products', productController.getAllProducts)
+app.get('/products/:id', productController.getProductById)
+app.post('/products', productController.createProduct)
+app.put('/products/:id', productController.updateProduct)
+app.delete('/products/:id', productController.deleteProduct)
+
+process.on('SIGINT', async () => {
+    await mongoose.connection.close();
+    console.log('Database connection closed');
+    process.exit(0);
+});

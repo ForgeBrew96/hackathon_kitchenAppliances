d55c1dede8e8671ffb0bc06f9ea7d4dd463f9016
ForgeBrew96 <christianbmendoza96@gmail.com>
2024-10-16 13:53:27 -0400

    Axios call works

--- client/script.js
+++ client/script.js
@@ -1,8 +1,15 @@
+const search = document.querySelector('.search-button')
+const input = document.getElementById('input')
+const productName = document.getElementById('productName'); 
+const productImg = document.getElementById('productImg'); 
+const productPrice = document.getElementById('productPrice'); 
+
 const getKitchenProducts = async () => {
     try {
         const response = await fetch('http://localhost:3001/products');
         const products = await response.json();
-        console.log(products);
+        console.log(products)
+        return products
     } catch (error) {
         console.error('Error fetching products:', error);
     }
@@ -10,3 +17,17 @@ const getKitchenProducts = async () => {
 
 getKitchenProducts();
 
+search.addEventListener('click', async () => {
+    const products = await getKitchenProducts();
+    const searchQuery = input.value.toLowerCase();
+    
+    const foundProduct = products.find(product => product.name.toLowerCase() === searchQuery);
+    
+    if (foundProduct) {
+        productName.textContent = foundProduct.name;
+        productImg.src = foundProduct.image
+        productPrice.textContent = `$${foundProduct.priceUSD}`
+    } else {
+        productName.textContent = 'Product not found';
+    }
+})
